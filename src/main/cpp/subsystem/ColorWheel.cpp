aff7cc3523fe797b59f8d3352cc4c9fa1d78aacb
Laxmonkey1000 <Laxmonkey1000@gmail.com>
2020-01-18 19:02:02 -0500

    Added Color Sensor

--- src/main/cpp/subsystem/ColorWheel.cpp
+++ src/main/cpp/subsystem/ColorWheel.cpp
@@ -5,18 +5,21 @@
 // Configure Hardware Settings
 ColorWheel::ColorWheel()
 {
-  
+   m_colorMatcher.AddColorMatch(kBlueTarget);
+   m_colorMatcher.AddColorMatch(kGreenTarget);
+   m_colorMatcher.AddColorMatch(kRedTarget);
+   m_colorMatcher.AddColorMatch(kYellowTarget);
 }
 
 // Stop all motors
 void ColorWheel::Stop()
 {
-    motorColorWheel.StopMotor();
+   motorColorWheel.StopMotor();
 }
 
 void ColorWheel::SetColorWheel(double speed)
 {
-   if(solenoidColorWheel.Get())
+   if (solenoidColorWheel.Get())
    {
       motorColorWheel.Set(speed);
    }
@@ -38,6 +41,30 @@ void ColorWheel::ColorWheelRetract()
 
 void ColorWheel::UpdateSmartdash()
 {
-    SmartDashboard::PutNumber("Motor ColorWheel", motorColorWheel.Get());
-    SmartDashboard::PutBoolean("Solenoid ColorWheel", solenoidColorWheel.Get());
+   SmartDashboard::PutNumber("Motor ColorWheel", motorColorWheel.Get());
+   SmartDashboard::PutBoolean("Solenoid ColorWheel", solenoidColorWheel.Get());
+   Color detectedColor = m_colorSensor.GetColor();
+
+   std::string colorString;
+    double confidence = 0.0;
+    Color matchedColor = m_colorMatcher.MatchClosestColor(detectedColor, confidence);
+
+    if (matchedColor == kBlueTarget) {
+      colorString = "Blue";
+    } else if (matchedColor == kRedTarget) {
+      colorString = "Red";
+    } else if (matchedColor == kGreenTarget) {
+      colorString = "Green";
+    } else if (matchedColor == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
+    }
+
+    SmartDashboard::PutNumber("Red", detectedColor.red);
+    SmartDashboard::PutNumber("Green", detectedColor.green);
+    SmartDashboard::PutNumber("Blue", detectedColor.blue);
+    SmartDashboard::PutNumber("Confidence", confidence);
+    SmartDashboard::PutString("Detected Color", colorString);
+
 }
--- src/main/include/subsystem/ColorWheel.hpp
+++ src/main/include/subsystem/ColorWheel.hpp
@@ -4,6 +4,8 @@
 #include <frc/PWMTalonSRX.h>
 #include <frc/SpeedControllerGroup.h>
 #include <frc/Solenoid.h>
+#include "rev/ColorSensorV3.h"
+#include "rev/ColorMatch.h"
 
 using namespace frc;
 using namespace ctre::phoenix::motorcontrol::can;
@@ -16,6 +18,17 @@ class ColorWheel
 
     // Solenoids
     Solenoid solenoidColorWheel{4};
+
+    //Color Sensor
+    static constexpr auto i2cPort = frc::I2C::Port::kOnboard;
+    rev::ColorSensorV3 m_colorSensor{i2cPort};
+    rev::ColorMatch m_colorMatcher;
+
+    //Color Presets
+    static constexpr frc::Color kBlueTarget = frc::Color(0.143, 0.427, 0.429);
+    static constexpr frc::Color kGreenTarget = frc::Color(0.197, 0.561, 0.240);
+    static constexpr frc::Color kRedTarget = frc::Color(0.561, 0.232, 0.114);
+    static constexpr frc::Color kYellowTarget = frc::Color(0.361, 0.524, 0.113);
 
   public:
     // Default Constructor
