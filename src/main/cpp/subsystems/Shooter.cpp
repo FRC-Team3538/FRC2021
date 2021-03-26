16b0262ce039127f2b9be03c6b4138b2a6545481
Jordan McMichael <jlmcmchl@gmail.com>
2021-03-25 23:43:27 -0400

    characterized + fleshed out controls more

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -14,6 +14,8 @@ using namespace frc;
 
 Shooter::Shooter()
 {
+  m_hoodEncoder.SetDistancePerPulse(0.04696044921875);
+
   m_shooterMotor1.ConfigFactoryDefault();
   m_shooterMotor1.SetInverted(true);
   m_shooterMotor1.SetNeutralMode(ctre::phoenix::motorcontrol::NeutralMode::Coast);
@@ -40,6 +42,40 @@ void Shooter::SimPeriodic()
 {
 }
 
+void Shooter::Periodic()
+{
+  // Hood
+  if (!m_hoodZeroed)
+  {
+    // TODO
+  } 
+  else
+  {
+    const auto hoodAngle = GetHoodAngle();
+
+    const auto hoodOutput = m_hoodPIDController.Calculate(hoodAngle, m_targetHoodAngle);
+    const auto hoodFf = m_hoodFeedforward.Calculate(m_hoodPIDController.GetSetpoint().velocity);
+
+    m_hoodVolts = units::volt_t{hoodOutput} + hoodFf;
+  }
+
+
+  // Shooter
+  const auto currentSpeed = GetShooterSpeed();
+
+  const auto shooterOutput = m_shooterPIDController.Calculate(currentSpeed, m_targetShooterSpeed);
+  const auto shooterFf = m_shooterFeedforward.Calculate(m_shooterPIDController.GetSetpoint().position, m_shooterPIDController.GetSetpoint().velocity);
+
+  m_shooterVolts = units::volt_t{shooterOutput} + shooterFf;
+
+  /******************************************************/
+
+  m_hoodMotor.SetVoltage(m_hoodVolts);
+
+  m_shooterMotor1.SetVoltage(m_shooterVolts);
+  m_shooterMotor2.SetVoltage(m_shooterVolts);
+}
+
 void Shooter::Log(UDPLogger &logger)
 {
   logger.LogExternalDevice(m_shooterMotor1);
@@ -54,17 +90,39 @@ void Shooter::InitSendable(frc::SendableBuilder &builder)
   builder.SetActuator(true);
 
   // Shooter
-  builder.AddDoubleProperty("Shooter Velocity [RPM@Motor]", [this] { return m_shooterMotor1.GetSelectedSensorVelocity(); }, nullptr);
+  builder.AddDoubleProperty(
+    "Shooter Velocity [RPM@Motor]", 
+    [this] { return GetShooterSpeed().value(); }, 
+    [this](double rpm) {m_targetShooterSpeed = units::revolutions_per_minute_t{rpm};});
+  builder.AddDoubleProperty(
+    "Shooter Voltage", 
+    [this] { return m_shooterMotor1.GetMotorOutputVoltage(); }, 
+    nullptr);
 
   // Gate
   // TODO
 
   // Hood
-  // TODO
+  builder.AddBooleanProperty(
+    "Hood Lower Limit Switch", 
+    [this] { return m_hoodLowerLimit.Get(); }, 
+    nullptr);
+  builder.AddDoubleProperty(
+    "Hood Angle", 
+    [this] {return GetHoodAngle().value();}, 
+    [this](double angle) {m_targetHoodAngle = units::degree_t{angle};});
+  builder.AddBooleanProperty(
+    "Hood Zeroed", 
+    [this] { return m_hoodZeroed;}, 
+    [this](bool zeroed) {m_hoodZeroed = zeroed;});
 
   // Thermal
   builder.AddDoubleProperty(
-      "Shooter 1 Temp [C]", [this] { return m_shooterMotor1.GetTemperature(); }, nullptr);
+    "Shooter 1 Temp [C]", 
+    [this] { return m_shooterMotor1.GetTemperature(); }, 
+    nullptr);
   builder.AddDoubleProperty(
-      "Shooter 2 Temp [C]", [this] { return m_shooterMotor2.GetTemperature(); }, nullptr);
+    "Shooter 2 Temp [C]", 
+    [this] { return m_shooterMotor2.GetTemperature(); }, 
+    nullptr);
 }
--- src/main/include/subsystems/Shooter.h
+++ src/main/include/subsystems/Shooter.h
@@ -26,6 +26,8 @@
 #include <frc/smartdashboard/SendableHelper.h>
 #include <lib/Loggable.hpp>
 
+#include <utility>
+
 // Vendor Libraries
 #include "ctre/Phoenix.h"
 #include "rev/CANSparkMax.h"
@@ -41,18 +43,80 @@ public:
 
     void Set(units::volt_t shooter, units::volt_t gate, units::volt_t hood);
 
+    void SetShooterSpeed(units::radians_per_second_t speed)
+    {
+        m_targetShooterSpeed = speed;
+    }
+
+    void SetHoodAngle(units::radian_t angle)
+    {
+        m_targetHoodAngle = angle;
+    }
+
+    void GotoSetpoint(int setpoint)
+    {
+        SetShooterSpeed(m_speed_setpoints[setpoint]);
+        SetHoodAngle(m_pitch_setpoints[setpoint]);
+    }
+
+    units::radians_per_second_t GetShooterSpeed()
+    {
+        return units::revolutions_per_minute_t{m_shooterMotor1.GetSelectedSensorVelocity() * 600.0 / 4096.0};
+    };
+    
+    units::radian_t GetHoodAngle()
+    {
+        return units::degree_t{m_hoodEncoder.GetDistance()};
+    };
+
+    void SetGate(units::volt_t gate)
+    {
+        m_gateMotor.SetVoltage(gate);
+    }
+
     void Log(UDPLogger &logger);
 
     void SimPeriodic();
-
+    void Periodic();
+    
     static constexpr auto kMaxShooterVoltage = 12_V;
     static constexpr auto kMaxGateVoltage = 12_V;
     static constexpr auto kMaxHoodVoltage = 12_V;
+
+    static constexpr auto kMaxShooterVelocity = 2900_rpm;
+    static constexpr auto kMaxHoodAngle = 60_deg;
 
 private:
     // Hardware
     WPI_TalonFX m_shooterMotor1{21};
     WPI_TalonFX m_shooterMotor2{22};
-    WPI_VictorSPX m_gateMotor{23};
-    WPI_VictorSPX m_hoodMotor{24};
+    WPI_VictorSPX m_hoodMotor{23};
+    WPI_VictorSPX m_gateMotor{24};
+
+    frc::Encoder m_hoodEncoder{1, 2, false, frc::CounterBase::EncodingType::k4X};
+    frc::DigitalInput m_hoodLowerLimit{4};
+
+    // MAX HOOD ANGLE: 60 deg
+    frc::ProfiledPIDController<units::radians_per_second> m_shooterPIDController{0.216, 0, 0, {1400_rad_per_s_sq, 6000_rad_per_s_sq / 1_s}};
+    frc::ProfiledPIDController<units::radians> m_hoodPIDController{0.133, 0, 0.000536, {110_deg_per_s, 20000_deg_per_s_sq}};
+
+    frc::SimpleMotorFeedforward<units::radians> m_shooterFeedforward{0.689_V, 0.228_V / 60_rpm, 0.0478_V / 60_rpm * 1_s};
+    frc::SimpleMotorFeedforward<units::radians> m_hoodFeedforward{0.983_V, 0.0927_V / 1_deg_per_s, 0.00039_V / 1_deg_per_s_sq};
+
+    units::volt_t m_shooterVolts = 0_V;
+    units::volt_t m_hoodVolts = 0_V;
+
+    units::radians_per_second_t m_targetShooterSpeed;
+    units::radian_t m_targetHoodAngle;
+
+    units::radian_t hoodPosition = 0_rad;
+    bool m_hoodZeroed = false;
+
+    const std::array<units::radians_per_second_t, 1> m_speed_setpoints{
+        0_rpm
+    };
+
+    const std::array<units::radian_t, 1> m_pitch_setpoints{
+        0_rad
+    };
 };
